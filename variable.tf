1844eb79b22e4d605e680f455ff7ba911c86a631
Marek Mościchowski <marek.moscichowski@miquido.com>
2021-06-18 09:50:39 +0200

    initial

---
+++ variable.tf
@@ -0,0 +1,105 @@
+variable "alb" {
+  type = object({
+    http_listener_arn  = string
+    https_listener_arn = string
+    alb_arn_suffix     = string
+    alb_dns_name       = string
+    alb_zone_id        = string
+  })
+  default     = null
+  description = "Alb module from ssh://git@gitlab.com/miquido/terraform/terraform-alb.git"
+}
+
+variable "ecs_cluster" {
+  type = object({
+    arn  = string
+    name = string
+  })
+  description = "resource aws_ecs_cluster where to deploy service"
+}
+
+variable "service_name" {
+  type        = string
+  default     = "prometheus"
+  description = "Name under which service will be deployed"
+}
+
+variable "vpc" {
+  type = object({
+    vpc_main_security_group_id = string
+    vpc_id                     = string
+    private_subnet_ids         = list(string)
+    vpc_main_security_group_id = string
+  })
+  description = "VPC module ssh://git@gitlab.com/miquido/terraform/terraform-vpc.git"
+}
+
+variable "domain" {
+  type        = string
+  description = "domain under which prometheus will be available. Required when alb is used"
+  default     = null
+}
+
+variable "ingress_priority" {
+  type        = number
+  default     = 89
+  description = "The priority for the rules without authentication, between 1 and 50000 (1 being highest priority). Must be different from `authenticated_priority` since a listener can't have multiple rules with the same priority"
+}
+
+variable "mesh_route53_zone_id" {
+  type        = string
+  default     = null
+  description = "mesh route id to create prometheus entry"
+}
+
+variable "route53_zone_id" {
+  type        = string
+  default     = null
+  description = "route id to create prometheus entry"
+}
+
+variable "project" {
+  type        = string
+  description = "Account/Project Name"
+}
+
+variable "environment" {
+  description = "Environment name"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "Default AWS Region"
+}
+
+variable "aws_appmesh_mesh_id" {
+  type    = string
+  default = null
+}
+
+variable "aws_service_discovery_private_dns_namespace" {
+  type = object({
+    name        = string
+    id          = string
+    hosted_zone = string
+  })
+  default = null
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Default tags to apply on all created resources"
+  default     = {}
+}
+
+variable "task_cpu" {
+  type        = number
+  default     = 512
+  description = "ECS task cpu for prometheus"
+}
+
+variable "task_memory" {
+  type        = number
+  default     = 1024
+  description = "ECS task memory for prometheus"
+}
