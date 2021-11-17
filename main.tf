ce4699a67e1fda25c80dbd96f4e52ae2c6e5416e
Miquido Ops <mqtc@miquido.com>
2021-11-17 05:05:58 +0000

    Bump ecs-alb-task-prometheus from 5.6.11 to 5.6.13
    Bumps [ecs-alb-task-prometheus](https://gitlab.com/miquido/terraform) from 5.6.11 to 5.6.13.
    - [Release notes](https://gitlab.com/miquido/terraform/tags)
    - [Commits](https://gitlab.com/miquido/terraform/compare/tags/5.6.11...tags/5.6.13)

--- main.tf
+++ main.tf
@@ -113,7 +113,7 @@ resource "aws_iam_role_policy" "service-discovery" {
 }
 
 module "ecs-alb-task-prometheus" {
-  source = "git::ssh://git@gitlab.com/miquido/terraform/terraform-ecs-alb-task.git?ref=tags/5.6.11"
+  source = "git::ssh://git@gitlab.com/miquido/terraform/terraform-ecs-alb-task.git?ref=tags/5.6.13"
 
   name                     = var.service_name
   project                  = var.project
