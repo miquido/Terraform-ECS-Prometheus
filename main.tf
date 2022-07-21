3e7ace4f6602ff01ec883c7070ee542604f47434
Miquido Ops <mqtc@miquido.com>
2022-07-21 06:37:28 +0000

    Bump alb-ingress-prometheus from 3.1.18 to 3.1.20

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ locals {
 
 module "alb-ingress-prometheus" {
   count       = var.alb != null ? 1 : 0
-  source      = "git::ssh://git@gitlab.com/miquido/terraform/terraform-alb-ingress.git?ref=3.1.18"
+  source      = "git::ssh://git@gitlab.com/miquido/terraform/terraform-alb-ingress.git?ref=3.1.20"
   name        = var.service_name
   project     = var.project
   environment = var.environment
